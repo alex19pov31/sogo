35bf64fb79f96169c4a8ee7e8cd5241df4291f07
Nesterov Alexandr <alex19pov31@gmail.com>
2025-11-05 16:22:21 +0300

    fix(timezone): update timezone

--- SoObjects/Appointments/SOGoAppointmentObject.m
+++ SoObjects/Appointments/SOGoAppointmentObject.m
@@ -2621,8 +2621,8 @@ inRecurrenceExceptionsForEvent: (iCalEvent *) theEvent
   
   rq = [_ctx request];
   NSString *myString = [rq contentAsString];
-  NSString *myStringEdited = [myString stringByReplacingOccurrencesOfString:@"TZOFFSETTO:+0231" withString:@"TZOFFSETTO:+0300"];
-  NSString *myStringEdited2 = [myStringEdited stringByReplacingOccurrencesOfString:@"TZOFFSETFROM:+0231" withString:@"TZOFFSETFROM:+0300"];
+  NSString *myStringEdited = [myString stringByReplacingOccurrencesOfString:@"TZOFFSETTO:+0400" withString:@"TZOFFSETTO:+0300"];
+  NSString *myStringEdited2 = [myStringEdited stringByReplacingOccurrencesOfString:@"TZOFFSETFROM:+0400" withString:@"TZOFFSETFROM:+0300"];
   //rqCalendar = [iCalCalendar parseSingleFromSource: [rq contentAsString]];
   rqCalendar = [iCalCalendar parseSingleFromSource: myStringEdited2];
   mustUpdate = YES;
